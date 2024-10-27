f0ca988e82e8fb043b2de098a94d850a39471bff
Jun <songjeongjun320@gmail.com>
2024-10-26 22:09:52 -0700

    main

--- front_end/src/controllers/Main_Pages_Controllers/CategoryController.java
+++ front_end/src/controllers/Main_Pages_Controllers/CategoryController.java
@@ -1,13 +1,30 @@
 package controllers.Main_Pages_Controllers;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class CategoryController {
+public class CategoryController extends Application {
+    
+    @Override
     public void start(Stage primaryStage) {
-    }
+        // "This is main page"
+        Label messageLabel = new Label("This is gonna be category controller");
+        messageLabel.setStyle("-fx-text-fill: white;"); // Text colors
         
+        StackPane layout = new StackPane(messageLabel);
+        layout.setStyle("-fx-background-color: #8b0000;"); // Wallpaper color
+        
+        // Scene
+        Scene scene = new Scene(layout, 800, 600); // 800x600
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Main Page");
+        primaryStage.show(); // Stage
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
-
 }
--- front_end/src/controllers/Main_Pages_Controllers/SearchBookController.java
+++ front_end/src/controllers/Main_Pages_Controllers/SearchBookController.java
@@ -11,9 +11,11 @@ public class SearchBookController extends Application {
     @Override
     public void start(Stage primaryStage) {
         // "This is main page"
-        Label messageLabel = new Label("This is main page");
+        Label messageLabel = new Label("This is gonna be main page");
+        messageLabel.setStyle("-fx-text-fill: white;"); // Text colors
         
         StackPane layout = new StackPane(messageLabel);
+        layout.setStyle("-fx-background-color: #8b0000;"); // Wallpaper color
         
         // Scene
         Scene scene = new Scene(layout, 800, 600); // 800x600
